d9b1f814e9ef96355f253eecd68ff87cac5e9d7d
Denzil M <denzilmuseruka@byui.edu>
2022-02-20 09:07:57 +0200

    Add project files.

---
+++ My Scripture Journal/Models/Scripture.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace My_Scripture_Journal.Models
+{
+    public class Scripture
+    {
+        public int Id { get; set; }
+        public string Book { get; set; }
+        public int Chapter { get; set; }
+        public int Verse { get; set; }
+        public string Notes { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+    }
+}
---
+++ My Scripture Journal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,68 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using My_Scripture_Journal.Data;
+using My_Scripture_Journal.Models;
+
+namespace My_Scripture_Journal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly My_Scripture_Journal.Data.My_Scripture_JournalContext _context;
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public IndexModel(My_Scripture_Journal.Data.My_Scripture_JournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Books { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Book { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
+        {
+            var scriptures = from m in _context.Scripture
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                scriptures = scriptures.Where(s => s.Book.Contains(SearchString));
+            }
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            ViewData["BookSortParm"] = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+    
+            switch (sortOrder)
+            {
+                case "book_desc":
+                    scriptures = scriptures.OrderByDescending(s => s.Book);
+                    break;
+                case "Date":
+                    scriptures = scriptures.OrderBy(s => s.DateAdded);
+                    break;
+                case "date_desc":
+                    scriptures = scriptures.OrderByDescending(s => s.DateAdded);
+                    break;
+                default:
+                    scriptures = scriptures.OrderBy(s => s.Book);
+                    break;
+            }
+
+            Scripture = await scriptures.ToListAsync();
+        }
+    }
+}
